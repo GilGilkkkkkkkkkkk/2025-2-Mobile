c233d439a5ccaccfad90fe15eba2c2a4deadd9f3
Gilvan <gilvankaua12@uni9.edu.br>
2025-08-28 01:00:04 +0000

    fish

--- main.dart
+++ main.dart
@@ -1,4 +1,20 @@
-voip(){
-    String nome = "jose";
-    print("Hola Mundo");
+import 'dart:io';
+
+void main(){
+
+    List<String?> alunos = [];
+    bool cadastrarOutroAluno = true;
+
+    while(cadastrarOutroAluno){
+        print("Informe nome do aluno:");
+        String? nome = stdin.readLineSync();
+
+        alunos.add(nome);
+
+        print('Deseja cadastrar outro aluno?(s/n)');
+        var repostaUsuario = stdin.readLineSync();
+        cadastrarOutroAluno = repostaUsuario == "s";
+        
+    };
+
 }
