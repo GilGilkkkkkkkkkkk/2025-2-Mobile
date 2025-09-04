16f1b49ec58abd20941d0da6226591c55fd93227
Gilvan <gilvankaua12@uni9.edu.br>
2025-09-04 00:42:35 +0000

    feat: aula-3/comando-else-if

---
+++ Exercício.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+
+    double Renda = 1400;
+
+    if( renda > 1400){
+        print("varejo");
+    }else if( renda > 5000){
+        print("varejo");
+    }else if( renda >= 30000){
+        print("vangogh");
+    }else if( renda >= 60000){
+        print("Select");
+    }else( renda >= 10000){
+        print("Saldo mair 10000");
+    }
+    
+}
